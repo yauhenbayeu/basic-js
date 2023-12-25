6adf00e073abd48e8323306370812913be2e7cd1
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2023-12-25 22:58:23 +0100

    feat: encoded string

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let encodedStr = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    }
+    else {
+      encodedStr += (count > 1 ? count : '') + str[i];
+      count = 1;
+    }
+  }
+
+  return encodedStr;
 }
 
 module.exports = {
