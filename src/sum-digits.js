47f83060a1dd158159c396351c1eae4200b514cb
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2023-12-25 23:41:28 +0100

    feat: digits & matrix

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  if (n < 10) {
+    return n;
+  }
+
+  let sum = 0;
+  const digitsArr = n.toString().split('');
+
+  digitsArr.forEach(digit => {
+    sum += parseInt(digit, 10);
+  })
+  
+  return getSumOfDigits(sum);
 }
 
 module.exports = {
