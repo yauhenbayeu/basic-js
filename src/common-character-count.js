b3ed658c384d247581ea610d0bfe0891facc04cd
Yauhen Bayeu <bayeu.yauhen@reliableopensource.com>
2023-12-25 22:17:51 +0100

    feat: common char & vinegere

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  let s2Arr = [...s2];
+
+  [...s1].forEach(char => {
+    if (s2Arr.indexOf(char) !== -1) {
+      count++;
+      s2Arr.splice(s2Arr.indexOf(char), 1);
+    }
+
+  })
+
+  return count
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+      if (message === undefined || key === undefined) {
+        throw new Error('Incorrect arguments!');
+      }
+      return this.cryptoProcess(message, key, 'encrypt');
+  }
+
+  decrypt(encryptedMessage, key) {
+      if (encryptedMessage === undefined || key === undefined) {
+        throw new Error('Incorrect arguments!');
+      }
+      return this.cryptoProcess(encryptedMessage, key, 'decrypt');
+  }
+
+  cryptoProcess(text, key, method) {
+    key = key.toUpperCase();
+    let result = '';
+    let keyIndex = 0;
+
+
+    // 
+    [...text].forEach(char => {
+      if (this.alphabet.includes(char.toUpperCase())) {
+        let shift = this.alphabet.indexOf(key[keyIndex % key.length]);
+        shift = method === 'encrypt' ? shift : -shift;
+        const newChar = this.shiftChar(char, shift);
+        result += newChar;
+        keyIndex++;
+      }
+      else {
+        result += char;
+      }
+    });
+
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  shiftChar(char, shift) {
+    const charIndex = this.alphabet.indexOf(char.toUpperCase());
+    const newIndex = (charIndex + shift + 26) % 26;
+    return this.alphabet[newIndex];
   }
 }
 
